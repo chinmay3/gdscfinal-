124ba0927b6d1f6da5ce596ba2c2667827f3a29f
chinmay3 <74144636+chinmay3@users.noreply.github.com>
2024-03-30 21:30:36 +0530

    Update Organizer.js

--- src/components/Organizer/Organizer.js
+++ src/components/Organizer/Organizer.js
@@ -1,88 +1,66 @@
-// import React, { Component } from "react";
-// import './Organizer.css';
-// import open from "../../images/openinn.png"; // Update the import path
+import React, { Component } from "react";
+import "./Organizer.css"; // Import custom CSS for the component
+import riyaImage from "../../images/riya.png";
+import tusharImage from "../../images/tushar.png";
 
-// import './team.css';
+class Organizer extends Component {
+  render() {
+    return (
+      <>
+        <div className="Past" id="Organizer">
+          <div className="ui centered grid">
+            <div className="Row">
+              <div className="Timeline">
+                <UncontrolledExample />
+              </div>
+            </div>
+            {/* <div className="Row">
+              <div className="Timeline">
+                <UncontrolledExample />
+              </div>
+            </div> */}
+          </div>
+        </div>
+      </>
+    );
+  }
+}
 
-// const organizersData = [
-//   {
-//     imgUrl: "https://cdn1.vectorstock.com/i/1000x1000/82/55/anonymous-user-circle-icon-vector-18958255.jpg",
-//     name: 'Organizer 1',
-//     position: 'Position 1',
-//     linkedin: 'linkedin-link',
-//     github: 'github-link',
-//     website: 'website-link'
-//   },
-//   {
-//     imgUrl: "https://cdn1.vectorstock.com/i/1000x1000/82/55/anonymous-user-circle-icon-vector-18958255.jpg",
-//     name: 'Organizer 1',
-//     position: 'Position 1',
-//     linkedin: 'linkedin-link',
-//     github: 'github-link',
-//     website: 'website-link'
-//   },
-//   {
-//     imgUrl: "https://cdn1.vectorstock.com/i/1000x1000/82/55/anonymous-user-circle-icon-vector-18958255.jpg",
-//     name: 'Organizer 1',
-//     position: 'Position 1',
-//     linkedin: 'linkedin-link',
-//     github: 'github-link',
-//     website: 'website-link'
-//   },
-//   {
-//     imgUrl: "https://cdn1.vectorstock.com/i/1000x1000/82/55/anonymous-user-circle-icon-vector-18958255.jpg",
-//     name: 'Organizer 1',
-//     position: 'Position 1',
-//     linkedin: 'linkedin-link',
-//     github: 'github-link',
-//     website: 'website-link'
-//   },
-//   // Add more organizers as needed
-// ];
+function UncontrolledExample() {
+  return (
+    <div className="slider">
+      <div className="slide-track">
+        <div className="slide">
+          <img src={riyaImage} alt="" className="slide-image" />
+        </div>
+        <div className="slide">
+          <img src={tusharImage} alt="" className="slide-image" />
+        </div>
+        <div className="slide">
+          <img src={riyaImage} alt="" className="slide-image" />
+        </div>
 
-// class Team extends Component {
-//   render() {
-//     return (
-//     <section className='our__team'>
-//       <div class="slider">
-// 	<div class="slide-track">
-// 		<div class="slide">
-// 			<img src={open} height="500" width="250" alt="" />
-// 		</div>
-// 		<div class="slide">
-// 			<img src={open} height="500" width="250" alt="" />
-// 		</div>
-// 		<div class="slide">
-// 			<img src={open} height="500" width="250" alt="" />
-// 		</div>
-// 		<div class="slide">
-// 			<img src={open} height="500" width="250" alt="" />
-// 		</div>
-// 		<div class="slide">
-// 			<img src={open} height="500" width="250" alt="" />
-// 		</div>
-// 		<div class="slide">
-// 			<img src={open} height="500" width="250" alt="" />
-// 		</div>
-// 		<div class="slide">
-// 			<img src={open} height="500" width="250" alt="" />
-// 		</div>
+        <div className="slide">
+          <img src={tusharImage} alt="" className="slide-image" />
+        </div>
+        <div className="slide">
+          <img src={riyaImage} alt="" className="slide-image" />
+        </div>
+        <div className="slide">
+          <img src={tusharImage} alt="" className="slide-image" />
+        </div>
+        <div className="slide">
+          <img src={riyaImage} alt="" className="slide-image" />
+        </div>
+        <div className="slide">
+          <img src={tusharImage} alt="" className="slide-image" />
+        </div>
+        <div className="slide">
+          <img src={riyaImage} alt="" className="slide-image" />
+        </div>
+      </div>
+    </div>
+  );
+}
 
-// 	</div>
-// </div>
-//     </section>
-//     );
-//   }
-// }
-
-// class Organizer extends Component {
-//   render() {
-//     return (
-//       <div className="Organizer">
-//         <Team />
-//       </div>
-//     );
-//   }
-// }
-
-// export default Organizer;
+export default Organizer;
